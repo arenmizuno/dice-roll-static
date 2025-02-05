e6adcfce5960ce0c4c04ab30fed2577d680d8bb0
Aren Mizuno <166631743+arenmizuno@users.noreply.github.com>
2025-02-05 19:57:05 +0000

    done

---
+++ app/controllers/dice_controller.rb
@@ -0,0 +1,29 @@
+class DiceController < ApplicationController
+  def two_six
+    render({ :template => "dice_templates/2d6"})
+  end
+
+  def two_ten
+    render({ :template => "dice_templates/2d10"})
+  end
+
+  def one_twenty
+    render({ :template => "dice_templates/1d20"})
+  end
+
+  def five_four
+    @dice_rolls = Array.new
+
+    5.times do
+      new_roll = rand(1..4)
+      @dice_rolls.push(new_roll)
+    end
+
+    render({ :template => "dice_templates/5d4"})
+  end
+
+  def home
+    render({ :template => "dice_templates/home"})
+  end
+
+end
